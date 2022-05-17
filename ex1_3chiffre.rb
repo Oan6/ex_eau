c425f3368cbb516f8fc371d70a2eb3dcad68a55e
Oan6 <maelsaby@sfr.fr>
2022-05-17 16:26:06 +0200

    d

--- ex1_3chiffre.rb
+++ ex1_3chiffre.rb
@@ -1,43 +1,54 @@
-#Créez un programme qui affiche toutes les différentes combinaisons possibles de trois chiffres dans l’ordre croissant, dans l’ordre croissant. La répétition est volontaire.
 
+$s = []
+def combinaison(x)		# faire les différante combi de 012 , ex 102 201.. 
+	x = x.to_s
+		a = x[0]
+		b = x[1]
+		c = x[2]
 
+	for i in 0..2
+		tps = b  
+		b = c  
+		c = tps 
 
-#pour rajouté un 0 de 0 a 99 
-a = []
-for i in 0..999 
-	a << i.to_s 
-end 
+		d = []
+		d << a.to_i  
+		d << b.to_i 
+		d << c.to_i 
+  		$s << d.join     
 
+  		d = []
+  		tps = a  
+		a = b  
+		b = tps 
+	
+		d << a.to_i  
+		d << b.to_i 
+		d << c.to_i
+		if d.join != x 
+			$s << d.join
+		end
+	end
+end  
 
+m = 0
+
+a = "012".."999"		
 for i in a 
-	if i.length != 3 
-		z = z.to_s + ["0" + i.to_s ].to_s
-	end
-end 
-#print z
+	m = 0
+	for z in $s 
+		if i == z 
+			m = m + 1
+		end 
+		if i[0] == i[1] or i[0] == i[2]		# si dans 909 le 1!= 2 ou 1!=3 
+			m = m + 1
+		elsif i[1] == i[2]					#ou sinon 2!=3 
+			m = m + 1 
+		end 
+	end 
+	if m == 0 
+		puts i 
+	end 
 
-for i in z 
-	if i.length != 3 
-		x = x.to_s + ["0" + i.to_s ].to_s
-	end
+	combinaison(i)			
 end 
-print x 
-#suprimer de 0 a 99 
-for i in a*(3/2)
-	if i.length != 3 
-		a.delete_at(0)
-	end
-end
-#print a
-#for i in $s 
-#	b << i
-#end 
-#puts b 
-#$d = []
-#for i in a 
-#	if i.length != 3 
-#		$d = $d.to_s + ["0" + i.to_s ].to_s
-#
-#	end 
-#end 
-#print $d
